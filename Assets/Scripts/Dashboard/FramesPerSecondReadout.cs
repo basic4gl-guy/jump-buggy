1b2d279dcc39c08ee4daead766b4ba4f54db2cdf
Tom Mulgrew <tmulgrew@slingshot.co.nz>
2018-12-29 16:10:24 +1300

    Dashboard control to display frames per second.

---
+++ Assets/Scripts/Dashboard/FramesPerSecondReadout.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FramesPerSecondReadout : MonoBehaviour {
+
+    public Text Readout;
+
+	// Update is called once per frame
+	void Update () {
+        var readout = Readout ?? GetComponent<Text>();
+        if (readout != null)
+            readout.text = (1.0f / Time.deltaTime).ToString("0");
+	}
+}
