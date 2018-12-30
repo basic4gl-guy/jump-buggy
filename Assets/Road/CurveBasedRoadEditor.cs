21bf6534492f6eb9990578573f1238367a0939d9
Tom Mulgrew <tmulgrew@slingshot.co.nz>
2018-12-31 12:22:00 +1300

    Add clear/override mesh/LOD functions for road curves.

--- Assets/Road/CurveBasedRoadEditor.cs
+++ Assets/Road/CurveBasedRoadEditor.cs
@@ -1,4 +1,4 @@
-﻿#if UNITY_EDITOR
+﻿#if UNITY_EDITOR    
 using UnityEditor;
 using UnityEngine;
 
@@ -6,10 +6,30 @@ using UnityEngine;
 public class CurveBasedRoadEditor : Editor {
     public override void OnInspectorGUI()
     {
+        CurveBasedRoad road = ((CurveBasedRoad)target);
         DrawDefaultInspector();
         if (GUILayout.Button("Update road"))
         {
-            ((CurveBasedRoad)target).RebuildMeshes();
+            road.RebuildMeshes();
+        }
+
+        GUILayout.Space(40);
+
+        if (GUILayout.Button("Override meshes"))
+        {
+            foreach (var curve in road.Curves)
+                if (curve.Mesh != null || curve.LODGroup != null)           // (Don't set mesh on jumps)
+                    curve.Mesh = road.OverrideMesh;
+        }
+        if (GUILayout.Button("Clear meshes"))
+        {
+            foreach (var curve in road.Curves)
+                curve.Mesh = null;
+        }
+        if (GUILayout.Button("Clear LOD groups"))
+        {
+            foreach (var curve in road.Curves)
+                curve.LODGroup = null;
         }
     }
 }
